2582e353e646e5a6e2a595075b4a72cdce131e89
get2knwshivansh <143176780+get2knwshivansh@users.noreply.github.com>
2025-03-13 15:39:06 +0530

    Create store.js

---
+++ store.js
@@ -0,0 +1,12 @@
+// src/store.js
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import productReducer from './reducers/productReducer';
+
+const rootReducer = combineReducers({
+  products: productReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
